eb42eb7e1eb6cabab570601705c7a98710dd9dc4
slam02 <exenn@hotmail.com>
2014-11-30 17:03:08 -0500

    Fixing env global issues

--- ubuntu_env_install.sh
+++ ubuntu_env_install.sh
@@ -3,17 +3,27 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-# see here : http://stackoverflow.com/questions/19352976/npm-modules-wont-install-globally-without-sudo
 sudo chown -R $USER /usr/local
 
-apt-get install vim-gnome
-apt-get install git
-apt-get install curl
+sudo apt-get install vim-gnome
+sudo apt-get install git
+sudo apt-get install curl
 
 
 # installing nodejs per https://github.com/joyent/node/wiki/installing-node.js-via-package-manager
 cd ~
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 
-apt-get install -y nodejs
-apt-get install -y build-essential
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+
+# because it seems there is no way to easily install
+# nodejs globally for this user...
+# see here : http://stackoverflow.com/questions/19352976/npm-modules-wont-install-globally-without-sudo
+mkdir -p ~/npm
+npm config set prefix ~/npm
+if ! grep -q 'export PATH="$PATH:$HOME/npm/bin"' .profile; then
+  cd ~
+  echo 'export PATH="$PATH:$HOME/npm/bin"' >> .profile
+fi
+. ~/.profile
