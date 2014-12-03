d73daedc9915d1f7f4e11a8ed96129c5f1515d89
JF <jean-francois.lavoie@keatonrow.com>
2014-12-02 19:04:33 -0500

    Using local version of node

--- ubuntu_env_install.sh
+++ ubuntu_env_install.sh
@@ -3,27 +3,28 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo chown -R $USER /usr/local
-
 sudo apt-get install vim-gnome
 sudo apt-get install git
 sudo apt-get install curl
 
 
-# installing nodejs per https://github.com/joyent/node/wiki/installing-node.js-via-package-manager
+# installing nodejs per https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
 cd ~
-curl -sL https://deb.nodesource.com/setup | sudo bash -
 
-sudo apt-get install -y nodejs
-sudo apt-get install -y build-essential
+sudo apt-get install build-essential libssl-dev
+curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh
+source ~/.profile
+source ~/.bashrc
+
+nvm install 0.10.26
 
 # because it seems there is no way to easily install
 # nodejs globally for this user...
 # see here : http://stackoverflow.com/questions/19352976/npm-modules-wont-install-globally-without-sudo
-mkdir -p ~/npm
-npm config set prefix ~/npm
-if ! grep -q 'export PATH="$PATH:$HOME/npm/bin"' .profile; then
-  cd ~
-  echo 'export PATH="$PATH:$HOME/npm/bin"' >> .profile
-fi
-. ~/.profile
+# mkdir -p ~/npm
+# npm config set prefix ~/npm
+# if ! grep -q 'export PATH="$PATH:$HOME/npm/bin"' .profile; then
+#  cd ~
+#  echo 'export PATH="$PATH:$HOME/npm/bin"' >> .profile
+# fi
+# . ~/.profile
